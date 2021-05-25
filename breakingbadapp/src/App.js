f69c04ea9eba52075719834d303ddf10daab48a5
Myles Burt <myles_burt@rocketmail.com>
2021-05-25 14:36:46 +0930

    Cards created to hold info from the Breaking Bad API and API info linked successfully. CSS copied and pasted from youtube tutorial repo.

--- breakingbadapp/src/App.js
+++ breakingbadapp/src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import Header from "./components/ui/Header"
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -23,6 +24,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items}  />
     </div>
   );
 }
