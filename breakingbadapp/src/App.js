b78fe2119cb026a79a2b8300416ce2853e0fe02d
Myles Burt <myles_burt@rocketmail.com>
2021-05-25 13:31:16 +0930

    Axios used to connect the Breaking BAd API with app.js. UseState and UseEffect used to bring api info into console.

--- breakingbadapp/src/App.js
+++ breakingbadapp/src/App.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Header from "./components/ui/Header"
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+        )
+      console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
       <Header />
@@ -10,3 +28,4 @@ const App = () => {
 }
 
 export default App;
+ 
